9599b2f6f83d6b2c603784f9dcd4585ca94ecc7d
ALEX <yugaialex9813@kbu.ac.kr>
2021-10-23 01:22:33 +0900

    third commit

--- javascript.js
+++ javascript.js
@@ -7,28 +7,68 @@ var appData = {
     timedata: time,
     expensise: {},
     opitonalexpensis: {},
-    icome:[],
-    saving: false
+    income:[],
+    saving: false,
+    chosseExpensess:  function(){
+        for (var i = 0; i < 2;i++){
+            var a = prompt("Введите обязательную статью расходов в этом месяце?",'');
+            b = prompt("Во сколько обойдется?",'');
+            if ((typeof(a)) == 'string' && (typeof(b)) == 'string' && a != null && b != null && a.length < 50){
+                console.log("done!");
+                appData.expensise[a] = b;
+              }else{
+                    i = i -1;
+              }
+    }
+   },
+   detectbudget: function(){
+    appData.moneyperday = appData.budget / 30;
+    alert("Ежедневный бюджет:" + appData.budget / 30);
+   },
+   detectlevel: function(){
+    if (appData.budget / 30 < 100){
+        console.log("Минимальный уровень достатка");
+   } else if (appData.budget / 30 >= 100 && appData.budget / 30 < 2000){
+       console.log("Средний уровень достатка");
+   }else if (appData.budget / 30 >= 2000) {
+       console.log("Высокий уровень достатка");
+   }
+   },
+   checksaving: function(){var save = +prompt("Какова сумма накоплений?");
+   var proce = +prompt("Каков процент от накоплений");
+   appData.monthincome = save/1000/12*proce;
+   alert("сумма накоплений за месяц: " + appData.monthincome);},
+  chooseOptExpenses: function(){
+    for(var i = 0; i <= 3;i++){
+        var statment = prompt("Статья необязательных расходов?");
+        appData.opitonalexpensis[i] = statment;
+       }
+  },
+  choseincome: function(){
+      var items = prompt('что принесет дополнительный доход? (перечислите через заптую)','');
+      if ((typeof(items) != 'string' || items == null)){
+          alert("error");
+      } else {
+      appData.income = items.split(', ');
+      appData.income.push(prompt("Может что-то еще?"));
+      appData.income.sort();
+      }
+      appData.income.forEach(function(items, i, mass){
+          alert("Способы дополнитьного заработка" + "способ " + (i+1)  + ": " + items);
+      });
+      for (var key in appData){
+          alert(key);
+      }
+  }
+
 };
 
- 
-for (var i = 0; i < 2;i++){
-    var a = prompt("Введите обязательную статью расходов в этом месяце?",'');
-    b = prompt("Во сколько обойдется?",'');
-    if ((typeof(a)) == 'string' && (typeof(b)) == 'string' && a != null && b != null && a.length < 50){
-        console.log("done!");
-        appData.expensise[a] = b;
-      }else{
-           
-      }
-}
-appData.moneyperday = appData.budget / 30;
- alert("Ежедневный бюджет:" + appData.budget / 30);
 
- if (appData.budget / 30 < 100){
-     console.log("Минимальный уровень достатка");
-} else if (appData.budget / 30 >= 100 && appData.budget / 30 < 2000){
-    console.log("Средний уровень достатка");
-}else if (appData.budget / 30 >= 2000) {
-    console.log("Высокий уровень достатка");
-}
+
+
+
+
+
+
+
+
